37db3e805fa56aeeb68636f3e6ecb1e0d6463809
an <an@xyz.com>
2024-01-27 12:46:09 +0530

    Replace the old car price prediction model with a new one.

--- app.py
+++ app.py
@@ -3,34 +3,7 @@ import numpy
 import pandas
 from  sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
-
-
-def predic(final_features):
-    df = pandas.read_csv("mappedData.csv")
-    df = df.dropna()
-    df = df.drop(["engine_fuel"],axis=1)
-    drop_list = ['УАЗ','Opel', 'Москвич', 'Dacia','Lancia','SsangYong', 'Daewoo','Geely', 'ВАЗ', 'Lifan', 'ЗАЗ','ГАЗ', 'Great Wall', 'Buick', 'Pontiac','Iveco',  'Saab', 'Infiniti', 'Chery', 'Peugeot']
-    for i in range(len(drop_list)):
-        df.drop(df.index[df["manufacturer_name"] == drop_list[i]],inplace=True)
-    outlairs = ["year_produced" , "odometer_value", "engine_capacity" , "price_usd"] 
-    for i in range(len(outlairs)):
-        Q1 = df[outlairs[i]].quantile(0.25)
-        Q3 = df[outlairs[i]].quantile(0.75)
-        IQR = Q3 - Q1
-        df = df[(df[outlairs[i]] >= Q1 - 1.5*IQR) & (df[outlairs[i]] <= Q3 + 1.5*IQR)]
-    df.drop_duplicates()
-    df["manufacturer_name"] = df["manufacturer_name"].astype(int)
-
-
-    x = df.drop("price_usd",axis=1)
-    y = df["price_usd"]
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.3, random_state = 42)
-
-    model = RandomForestRegressor(n_estimators=9 , random_state=42)
-    model.fit(X_train,y_train)
-    return (model.predict([final_features])[0])
-    
-
+import model
 
 
 app = Flask(__name__)
@@ -42,15 +15,11 @@ def apply_caching(response):
     return response
 
 
-
 app.config.update(
     SESSION_COOKIE_SECURE=True,
     SESSION_COOKIE_HTTPONLY=True,
     SESSION_COOKIE_SAMESITE='Lax',
 )
-
-
-
 
 
 @app.route("/", methods = ["GET","POST"])
@@ -134,7 +103,7 @@ def predict():
         final_features.append(l2.index(f10))
 
 
-        output = predic(final_features)
+        output = model.predic(final_features)
         
         return render_template('index.html',prediction_text="{:.2f}".format(output))
         
